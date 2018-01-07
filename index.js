827103ca35db09d705285329153f99faf6f84854
Augusto Cesar <guto_slv@live.com>
2018-01-07 00:22:19 -0200

    Adds score variables and count then on the round function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+//Create global variables related to user
 const weapons = ['Rock', 'Paper', 'Scissors'];
 let playerSelection = computerPlay();
 let computerSelection = computerPlay();
+
+//Create global variables related to playGame() function
+const maxRound = 5
+let gameNumber = 0
+let roundNumber = 0
+let playerScore = 0
+let computerScore = 0
+let tieScore = 0
 
 //Creates the function to make the computer play
 function computerPlay() {
@@ -15,35 +24,54 @@ function playRound(playerSelection, computerSelection) {
 
     if(playerSelection === computerSelection) {
         result = `You both selected ${computerSelection}, it's a tie.`;
+        tieScore++;
+        roundNumber++;
         return result;
         }
     else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         result = 'Rock beats Scissors. Player wins.';
+        playerScore++;
+        roundNumber++;
         return result;
     }
     else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         result = 'Paper beats rock. Computer wins.';
+        computerScore++;
+        roundNumber++;
         return result;
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         result = 'Paper beats rock. Player wins.';
+        playerScore++;
+        roundNumber++;
         return result;
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         result = 'Scissors beats paper. Computer wins.';
+        computerScore++;
+        roundNumber++;
         return result;
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         result = 'Scissors beats paper. Player wins.';
+        playerScore++;
+        roundNumber++;
         return result;
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         result = 'Rock beats Scissors. Computer wins.';
+        computerScore++;
+        roundNumber++;
         return result;
     }
     else {
         alert('Please insert Rock, Papers or Scissors!')
     }
+}
+
+//Creates the function to play a game
+function playGame() {
+
 }
 
 console.log(playRound(playerSelection, computerSelection))
